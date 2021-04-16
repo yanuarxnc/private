d76937b3450af9bce2a9c05d66ffd4647571ce50
Yanuar Nurcahyo <yanuarxproject@gmail.com>
2021-04-16 12:55:12 +0700

    bubble fail

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBubble(t *testing.T) {
+
+	testCases := map[string]struct {
+		a      []int
+		n      int
+		expect []int
+	}{
+		"Test 1": {
+			a:      []int{7, 4, 5, 2},
+			n:      4,
+			expect: []int{2, 3, 5, 7},
+		},
+	}
+
+	for name, test := range testCases {
+
+		t.Run(name, func(t *testing.T) {
+			output := Bubble(test.a, test.n)
+			assert.Equal(t, test.expect, output)
+		})
+	}
+}
